5e502a3a298dc26d1d1329b926a4a7aa33d088bb
F4HH0oF <alekzanderignatov@gmail.com>
2023-04-16 17:28:42 +0300

    Part 1- basics and theme understanding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Typography } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
+import styled from "@emotion/styled";
+const App = () => {
+	const BlueButton = styled(Button)({
+		backgroundColor: "skyblue",
+		color: "#888",
+		margin: 5,
+		"&:hover": {
+			backgroundColor: "lightblue",
+		},
+		"&:disabled": {
+			backgroundColor: "gray",
+			color: "white",
+		},
+	});
+	return (
+		<div>
+			<Button variant="text">Text</Button>
+			<Button startIcon={<Settings />} variant="contained" color="secondary">
+				Settings
+			</Button>
+			<Button startIcon={<Add />} variant="contained" color="success">
+				Add new post
+			</Button>
+			<Button variant="outlined" size="small">
+				Outlined
+			</Button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+			<Typography variant="h1" component="p">
+				It uses h1 style but is p tag.
+			</Typography>
+
+			<BlueButton>My unique button 1</BlueButton>
+			<BlueButton>My unique button 2</BlueButton>
+		</div>
+	);
+};
 
 export default App;
